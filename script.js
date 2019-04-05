f39fafa89eeefe0c6f0fc65a3d0b1ad1390af1a0
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-04-05 00:43:55 +0000

    added shell files for goodreads api

---
+++ script.js
@@ -0,0 +1,12 @@
+function search(){
+	const keyword = document.getElementById("keyword").value;
+	 console.log(keyword);
+	 //const url = 
+	 //get results from API using fetch
+	 const endpoint = 'https://www.goodreads.com/search/index.xml'
+	 fetch(url)
+	 .then(function(response){
+	 	
+	 })
+	 //Display results on page as a list (<li></li>)
+}
